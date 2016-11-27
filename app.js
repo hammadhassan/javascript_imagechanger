2bb0428fe6a411fef91bd0aea3ca714e348d17fb
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-11-28 00:37:33 +0500

    update slogan

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ function random() {
     var randomnum = Math.random();
     var phone = ((randomnum * 15) + 1);
     var phonenum = Math.floor(phone);
-    num.push(phonenum);
+    num.push(phonenum);    
+    //num = [];
 }
 
-switch(num) {
+var number = num;
+switch(number) {
     case 1 :
     document.getElementById("one").innerHTML = ("Original-iphone" + "<img src='images/iphone-original.jpg' width='538' height='406'>");
     break;
@@ -15,37 +17,37 @@ switch(num) {
     document.getElementById("one").innerHTML= ("Iphone 3G" + "<img src='images/iphone3g.jpg' width='538' height='406'>");
     break;
     case 3 :
-    document.getElementById("one").innerHTML= ("Iphone 3GS" + "<img src='images/iphone3gs.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML= ("Iphone 3GS 'The fastest, smartest phone yet'" + "<img src='images/iphone3gs.jpg' width='538' height='406'>");
     break;
     case 4 :
-    document.getElementById("one").innerHTML = ("Iphone 4" + "<img src='images/iphone4.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone 4 'This changes everything. Again'" + "<img src='images/iphone4.jpg' width='538' height='406'>");
     break;
     case 5 :
-    document.getElementById("one").innerHTML = ("Iphone 4s" + "<img src='images/iphone4s.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone 4s 'The most amazing iPhone yet'" + "<img src='images/iphone4s.jpg' width='538' height='406'>");
     break;
     case 6 :
-    document.getElementById("one").innerHTML = ("Iphone 5" + "<img src='images/iphone5.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone 5 'The biggest thing to happen to iPhone since iPhone'" + "<img src='images/iphone5.jpg' width='538' height='406'>");
     break;
     case 7 :
-    document.getElementById("one").innerHTML = ("Iphone 5s" + "<img src='images/iphone5s.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone 5s 'Forward Thinking" + "<img src='images/iphone5s.jpg' width='538' height='406'>");
     break;
     case 8 :
-    document.getElementById("one").innerHTML = ("Iphone 5c" + "<img src='images/iphone5c.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone 5c 'For the colorful'" + "<img src='images/iphone5c.jpg' width='538' height='406'>");
     break;
     case 9 :
-    document.getElementById("one").innerHTML = ("Iphone SE" + "<img src='images/iphonese.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone SE 'A big step for small'" + "<img src='images/iphonese.jpg' width='538' height='406'>");
     break;
     case 10 :
-    document.getElementById("one").innerHTML = ("Iphone 6" + "<img src='images/iphone6.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone 6 'The two and only'" + "<img src='images/iphone6.jpg' width='538' height='406'>");
     break;
     case 11 :
-    document.getElementById("one").innerHTML = ("Iphone 6 Plus" + "<img src='images/iphone6plus.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("Iphone 6 Plus 'Bigger than bigger'" + "<img src='images/iphone6plus.jpg' width='538' height='406'>");
     break;
     case 12 :
-    document.getElementById("one").innerHTML = ("'The two and only thing' Iphone 6s" + "<img src='images/iphone6s.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = (" Iphone 6s 'The only thing that's changed is everything'" + "<img src='images/iphone6s.jpg' width='538' height='406'>");
     break;
     case 13 :
-    document.getElementById("one").innerHTML = ("'The two and only thing'Iphone 6s Plus" + "<img src='images/iphone6splus.jpg' width='538' height='406'>");
+    document.getElementById("one").innerHTML = ("'Iphone 6s Plus 'The only thing that's changed is everything'" + "<img src='images/iphone6splus.jpg' width='538' height='406'>");
     break;
     case 14 :
     document.getElementById("one").innerHTML = ("This is 7" + "<img src='images/iphone7.jpg' width='538' height='406'>");
